ffe61bdd21c216f74559318a592d9db8e76ad508
odunola <jenrola2292@gmail.com>
2025-11-05 12:22:19 +0100

    initial commit

--- tutorials/launch_encoders.py
+++ tutorials/launch_encoders.py
@@ -3,11 +3,13 @@ from latentlm.vae.audio.model import SemanticTokenizerModel, AcousticTokenizerMo
 import torch
 from torch import nn
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 semantic_config = SemanticTokenizerConfig()
 acoustic_config = AcousticTokenizerConfig()
 
-semantic_model = SemanticTokenizerModel(semantic_config)
-acoustic_model = AcousticTokenizerModel(acoustic_config)
+semantic_model = SemanticTokenizerModel(semantic_config).to(device)
+acoustic_model = AcousticTokenizerModel(acoustic_config).to(device)
 
 semantic_params = [p.numel() for p in semantic_model.parameters()]
 acoustic_params = [p.numel() for p in acoustic_model.parameters()]
@@ -15,8 +17,11 @@ acoustic_params = [p.numel() for p in acoustic_model.parameters()]
 print(f"Num of Semantic_params: {sum(semantic_params)}")
 print(f"Num of acoustic params: {sum(acoustic_params)}")
 
-tensor = torch.randn((1,1,24000))
+tensor = torch.randn((1,1,24000), device = device)
 print(tensor.shape)
 
 output = semantic_model(tensor, debug = True)
+print(output)
+
+output = acoustic_model(tensor, debug = True)
 print(output)
